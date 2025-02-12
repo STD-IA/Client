e12ac2059504dc33dc89aa0137aed094ba21c61f
Ruan Sanchez <ruansanchez@MacBook-Air-de-Ruan.local>
2025-02-11 22:50:21 -0300

    projeto finalizado

--- client.py
+++ client.py
@@ -8,14 +8,15 @@ from logging.handlers import RotatingFileHandler
 from multiprocessing import Process, shared_memory, Manager
 from xmlrpc.server import SimpleXMLRPCRequestHandler, SimpleXMLRPCServer
 
-inicio, task_memoria_comp, servidores_memoria_comp, lock = 0,0,0,0
-log_file = "treinamento.log"
+tempo_inicio, task_memoria_comp, servidores_memoria_comp, lock = 0,0,0,0
+log_file = "processo_treinamento.log"
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2')
 
+#Registra um servidor e inicia um processo de treinamento nele
 def cadastrar_servidor(ip, porta):
-    srv = (ip, porta)
+    server = (ip, porta)
     with lock:
         servidores = ler_memoria_compartilhada(servidores_memoria_comp)
         try:
@@ -23,8 +24,9 @@ def cadastrar_servidor(ip, porta):
             escrever_memoria_compartilhada(servidores_memoria_comp, servidores)
         except:
             return "Erro: Não é possível registrar mais servidores."
-    Process(target=treinar, args=(srv[0], srv[1], task_memoria_comp.name, servidores_memoria_comp.name, lock, log_file)).start()
-    return "Server registered."
+
+    Process(target=treinar, args=(server[0], server[1], task_memoria_comp.name, servidores_memoria_comp.name, lock, log_file)).start()
+    return "Servidor registrado e inicializando o processo"
 
 def log_config(log_file):
     logger = logging.getLogger()
@@ -42,25 +44,26 @@ def log_config(log_file):
 
 def inicializar_memoria_compartilhada(data):
     data_json = json.dumps(data)
-    shm = shared_memory.SharedMemory(create=True, size=len(data_json))
-    shm.buf[:len(data_json)] = data_json.encode('utf-8')
-    return shm
+    memoria_compartilhada = shared_memory.SharedMemory(create=True, size=len(data_json))
+    memoria_compartilhada.buf[:len(data_json)] = data_json.encode('utf-8')
+    return memoria_compartilhada
 
-def ler_memoria_compartilhada(shm):
-    data_json = bytes(shm.buf[:shm.size]).decode('utf-8').rstrip('\x00')  # Remove dados residuais
+def ler_memoria_compartilhada(memoria_compartilhada):
+    data_json = bytes(memoria_compartilhada.buf[:memoria_compartilhada.size]).decode('utf-8').rstrip('\x00')  # Remove dados residuais
     return json.loads(data_json)
 
-def escrever_memoria_compartilhada(shm, data):
+def escrever_memoria_compartilhada(memoria_compartilhada, data):
     data_json = json.dumps(data)
-    if len(data_json) > shm.size:
+    if len(data_json) > memoria_compartilhada.size:
         raise ValueError("O tamanho do JSON excede o tamanho da memória compartilhada!")
-    shm.buf[:len(data_json)] = data_json.encode('utf-8')
-    shm.buf[len(data_json):] = b'\x00' * (shm.size - len(data_json))  # Limpa o buffer residual
+    memoria_compartilhada.buf[:len(data_json)] = data_json.encode('utf-8')
+    memoria_compartilhada.buf[len(data_json):] = b'\x00' * (memoria_compartilhada.size - len(data_json))  # Limpa o buffer residual
 
 def treinar(ip, porta, task_memoria_comp_name, servidores_memoria_comp_name, lock, log_file):
     logger = log_config(log_file)
     task_memoria_comp = shared_memory.SharedMemory(name=task_memoria_comp_name)
     servidores_memoria_comp = shared_memory.SharedMemory(name=servidores_memoria_comp_name)
+
     while True:
         with lock:
             tasks = ler_memoria_compartilhada(task_memoria_comp)
@@ -69,7 +72,7 @@ def treinar(ip, porta, task_memoria_comp_name, servidores_memoria_comp_name, loc
                 servidores.remove([ip,porta])
                 if not servidores:
                     fim = time.time()
-                    duracao = fim - inicio
+                    duracao = fim - tempo_inicio
                     logger.info(f"Treinamento concluído. Duração: {duracao}s")
                     task_memoria_comp.close()
                     task_memoria_comp.unlink()
@@ -112,10 +115,13 @@ if __name__ == "__main__":
     weight_decays = [float(e) for e in config['Params']['weight_decays'].split(', ')]
 
     # Configura os argumentos para cada treinamento
-    tasks = [
-        (model_name, epoch, learning_rate, weight_decay, replicacoes)
-        for model_name in model_names for epoch in epochs for learning_rate in learning_rates for weight_decay in weight_decays
-    ]
+    tasks = []
+    for model_name in model_names:
+        for epoch in epochs:
+            for learning_rate in learning_rates:
+                for weight_decay in weight_decays:
+                    tasks.append((model_name, epoch, learning_rate, weight_decay, replicacoes))
+
 
     # Inicializar memória compartilhada para tasks
     task_memoria_comp = inicializar_memoria_compartilhada(tasks)
@@ -137,6 +143,6 @@ if __name__ == "__main__":
     print(f"Iniciando servidor em {ip}:{porta}...")
     server = SimpleXMLRPCServer((ip, porta), requestHandler=RequestHandler)
     server.register_function(cadastrar_servidor)
-    print("Servidor iniciado.")
-    inicio = time.time()
+    print("Cliente iniciado.")
+    tempo_inicio = time.time()
     server.serve_forever()
